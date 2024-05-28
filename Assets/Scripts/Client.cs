d47646a928b8414874b7a149a837733bb5b0184a
우종현 <whdgus616@gmail.com>
2024-05-28 16:57:56 +0900

    클라이언트 서버 접속

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Client : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    public Button sendButton;
+    public TextMeshProUGUI receivevdMeeageText;
+
+    private TcpClient client;
+    private NetworkStream stream;
+    private byte[] buffer = new byte[1024];
+    public string serverAddress = "127.0.0.1";
+    public int port = 8888;
+
+    void Start()
+    {
+        ConnectToServer();
+
+    }
+
+    
+    private void ConnectToServer()
+    {
+        try
+        {
+            client = new TcpClient(serverAddress, port);
+            stream = client.GetStream();
+            Debug.Log("서버 접속");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
+    }
+
+    
+    private void OnApplicationQuit()
+    {
+        if (stream != null)
+        {
+            stream.Close();
+        }
+        if (client != null)
+        {
+            client.Close();
+        }
+    }
+}
